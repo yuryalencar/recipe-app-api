dc8e38ff91aed4578c5f76ddb8b80d4d9386dd5f
Yury Alencar <yuryalencar19@gmail.com>
2023-07-02 19:12:54 -0300

    refactor: serializer order

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -78,11 +78,12 @@ class RecipeSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         """Update a recipe."""
         tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+
         if tags is not None:
             instance.tags.clear()
             self._get_or_create_tags(tags, instance)
 
-        ingredients = validated_data.pop('ingredients', None)
         if ingredients is not None:
             instance.ingredients.clear()
             self._get_or_create_ingredients(ingredients, instance)
