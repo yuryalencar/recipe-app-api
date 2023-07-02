ac8fec912df16f46739d55ba6f9af9bbcce5ddfe
Yury Alencar <yuryalencar19@gmail.com>
2023-07-02 01:07:48 -0300

    added user manage endpoints

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,8 +18,19 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
-        """Create and return a user with encrypted password."""
+        """Create and return a user."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return a user with encrypted password."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
