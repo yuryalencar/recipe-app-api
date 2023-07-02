60fb6f769d2bb10145b945f6a217d906ae26e74f
Yury Alencar <yuryalencar19@gmail.com>
2023-07-02 14:33:09 -0300

    feat(tags): added tags list

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,7 +2,10 @@
 Serializers for Recipe APIs.
 """
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import (
+    Tag,
+    Recipe,
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -19,3 +22,12 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
